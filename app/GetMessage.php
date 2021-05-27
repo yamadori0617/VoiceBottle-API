656c271d208299a18b73fda26a713c0d4d19d3c1
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-05-27 16:13:09 +0900

    Update GetMessage.php

--- app/GetMessage.php
+++ app/GetMessage.php
@@ -7,11 +7,11 @@ use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Model;
 class GetMessage extends Model
 {
     protected $fillable = [
-        'id', 'from_id', 'latest_audio_path',
+        'id', 'from_id',
     ];
 
     protected $hidden = [
-        'id', 'from_id', 'latest_audio_path',
+        'id', 'from_id',
     ];
 
 }
