9c4b111f47b081f77c5f92f02d752d073dcb6704
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-05-27 15:41:57 +0900

    Update RegisterController.php

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\User;
-
+use ErrorException;
 
 class RegisterController extends BaseController
 {
@@ -26,18 +25,23 @@ class RegisterController extends BaseController
         $password = strval($this->randomPassword());
         $api_token = Str::random(60);
 
-        if ($name == null) {
-            $user = User::create([
-                'password' => $password,
-                'api_token' => $api_token,
-            ]);
-            $user['name'] = "匿名さん";
-        } else {
-            $user = User::create([
-                'name' => $name,
-                'password' => $password,
-                'api_token' => $api_token,
-            ]);    
+        try {
+            if ($name == null) {
+                $user = User::create([
+                    'password' => $password,
+                    'api_token' => $api_token,
+                ]);
+                $user['name'] = "匿名さん";
+            } else {
+                $user = User::create([
+                    'name' => $name,
+                    'password' => $password,
+                    'api_token' => $api_token,
+                ]);    
+            }
+    
+        } catch (ErrorException $e) {
+            return $this->sendError('Query Error');
         }
 
         $success['id'] = $user['id'];
@@ -50,12 +54,12 @@ class RegisterController extends BaseController
 
     public function randomPassword() {
         $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-        $pass = array(); //remember to declare $pass as an array
-        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        $pass = array();
+        $alphaLength = strlen($alphabet) - 1;
         for ($i = 0; $i < 10; $i++) {
             $n = rand(0, $alphaLength);
             $pass[] = $alphabet[$n];
         }
-        return implode($pass); //turn the array into a string
+        return implode($pass);
     }     
 }
